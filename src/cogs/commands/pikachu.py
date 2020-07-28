17529a1460331aea88fc31c55f74682d04040b18
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-07-28 23:09:42 +0200

    Pikachu command

---
+++ src/cogs/commands/pikachu.py
@@ -0,0 +1,22 @@
+from discord import Embed
+from discord.ext import commands
+
+from src.modules.random import get_link_from_API
+
+
+# We need some pika pika in our lives
+class Pikachu(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='pikuachu', aliases=['pika'])
+    async def pikachu(self, ctx):
+        await ctx.message.delete()
+        await ctx.send(embed=Embed(description="⊹　 ✺ * ·　Pika~ Pika~..",
+                                   color=0x2F3136)
+                       .set_image(url=await get_link_from_API('https://some-random-api.ml/img/pikachu')))
+
+
+def setup(bot):
+    bot.add_cog(Pikachu(bot))
--- src/modules/random.py
+++ src/modules/random.py
@@ -30,3 +30,12 @@ async def get_random_joke_text():
                                "https://official-joke-api.appspot.com/random_joke")
         await session.close()
         return response["setup"], response["punchline"]
+
+
+# Retrieving image from API
+async def get_link_from_API(api):
+    async with aiohttp.ClientSession() as session:
+        response = await fetch(session,
+                               api)
+        await session.close()
+        return response["link"]
