647f9ec1d1c771c98663e82e362ed2676327c329
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-07-28 17:33:38 +0200

    Avatar cmd

---
+++ src/cogs/commands/avatar.py
@@ -0,0 +1,32 @@
+from discord import User, Message
+from discord.ext.commands import BadArgument, MissingRequiredArgument
+
+from discord.ext import commands
+
+from src.modules.random import get_random_gif_by_theme
+
+
+# My avatar is beautifuler than yours, or maybe you just want to look at another one's avatar ?
+class Avatar(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='avatar')
+    async def avatar(self, ctx, target: User):
+        await ctx.message.delete()
+        if ctx.message.author != target:
+            await ctx.send(f"{ctx.message.author.mention}, that's the avatar of **{target.name}**\n {ctx.message.author.avatar_url_as(size=4096)}")
+        else: await ctx.send(f"{ctx.message.author.mention}, that's your avatar !\n {ctx.message.author.avatar_url_as(size=4096)}")
+
+    @avatar.error
+    async def avatar_error(self, ctx, error):
+        await ctx.message.delete()
+        if isinstance(error, BadArgument):
+            await ctx.send("Erm~, sorry- I can't find that member...", delete_after=5.0)
+        elif isinstance(error, MissingRequiredArgument):
+            await ctx.send(f"{ctx.message.author.mention}, that's your avatar !\n {ctx.message.author.avatar_url_as(size=4096)}")
+
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))
--- src/cogs/commands/hug.py
+++ src/cogs/commands/hug.py
@@ -1,5 +1,5 @@
 from discord import User
-from discord.ext.commands import MissingRequiredArgument
+from discord.ext.commands import MissingRequiredArgument, BadArgument
 
 from discord import Embed
 from discord.ext import commands
@@ -20,12 +20,13 @@ class Hug(commands.Cog):
                                    color=0x2F3136)
                        .set_image(url=f"{await get_random_gif_by_theme('anime hug')}"))
 
-    @staticmethod
     @hug.error
-    async def hug_error(ctx, error):
+    async def hug_error(self, ctx, error):
         await ctx.message.delete()
         if isinstance(error, MissingRequiredArgument):
             await ctx.send("Please specify someone to hug on, it would be so sad otherwise ! 😢", delete_after=5.0)
+        elif isinstance(error, BadArgument):
+            await ctx.send("Erm~, sorry- I can't find that member...", delete_after=5.0)
 
 
 def setup(bot):
