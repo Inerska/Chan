2714b53cf899db7491c52637e3f6d30292d0ec56
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-07-28 16:02:53 +0200

    urllib -> aiohttp

--- src/modules/random.py
+++ src/modules/random.py
@@ -1,10 +1,13 @@
 import random
-import json
-from urllib.request import urlopen
+import aiohttp
 
-from src.utils import get_key_from_json
+from src.utils import get_key_from_json, fetch
 
 
-def get_random_gif_by_theme(theme):
-    response = json.load(urlopen(f"http://api.giphy.com/v1/gifs/search?q={theme}&api_key={get_key_from_json('GIPHY_API_KEY')}&limit=10&sort=revelant&rating=g"))
-    return response["data"][random.randint(0, 9)]["embed_url"]
+# Retrieving gifs by theme query from Giphy API
+async def get_random_gif_by_theme(theme: str):
+    async with aiohttp.ClientSession() as session:
+        response = await fetch(session,
+                               f"http://api.giphy.com/v1/gifs/search?q={theme.replace(' ', '+')}&api_key={get_key_from_json('GIPHY_API_KEY')}&limit=10&sort=revelant&rating=g")
+        await session.close()
+        return response["data"][random.randint(0, 9)]["images"]["downsized"]["url"]
