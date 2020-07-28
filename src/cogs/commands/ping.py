ebf1b7e7da644587e951bb714795bfc3e30e841e
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-07-28 14:10:56 +0200

    Delete user command message

--- src/cogs/commands/ping.py
+++ src/cogs/commands/ping.py
@@ -9,6 +9,7 @@ class Ping(commands.Cog):
 
     @commands.command(name='ping')
     async def ping(self, ctx):
+        await ctx.message.delete()
         await ctx.send(embed=discord.Embed(color=0x2F3136,
                                            description=f"Chan's ping is `{int(self.bot.latency * 1000)} ms` !"))
 
