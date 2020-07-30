150f5a55d6e9f8b61d3ce6a549f7cccabc134857
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-07-30 16:57:36 +0200

    Flip command

---
+++ src/cogs/commands/flip.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from random import choice
+
+
+# Heads or Tails !
+class Flip(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='flip', aliases=['coinflip', 'headsortails'])
+    async def flip(self, ctx):
+        await ctx.message.delete()
+        await ctx.send(embed=discord.Embed(color=0x2F3136,
+                                           description=f"`{choice(['Heads', 'Tails'])}`"))
+
+
+def setup(bot):
+    bot.add_cog(Flip(bot))
