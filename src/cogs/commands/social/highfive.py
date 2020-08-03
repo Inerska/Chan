379ad7cd80f9ee1cdeb65b1222787ef7763da9e0
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-08-03 18:40:15 +0200

    Highfive social command

---
+++ src/cogs/commands/social/highfive.py
@@ -0,0 +1,38 @@
+from discord import User
+from discord.ext.commands import MissingRequiredArgument, BadArgument
+
+from discord import Embed
+from discord.ext import commands
+
+from src.modules.random import get_random_gif_by_theme
+
+
+# Yay, we did it !
+from src.utils import safe_delete
+
+
+class HighFive(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='highfive', aliases=['high5', 'h5'])
+    async def highfive(self, ctx, target: User):
+        await safe_delete(ctx)
+        if ctx.message.author != target:
+            await ctx.send(embed=Embed(description=f"(._.)/(._. **{ctx.author.name}** highfives **{target.name}**",
+                                       color=0x2F3136)
+                           .set_image(url=f"{await get_random_gif_by_theme('anime highfive')}"))
+        else: await ctx.send("You really want to this alone-.. ?", delete_after=5.0)
+
+    @highfive.error
+    async def highfive_error(self, ctx, error):
+        await safe_delete(ctx)
+        if isinstance(error, MissingRequiredArgument):
+            await ctx.send("Please specify someone to highfive with, it would be so sad otherwise ! 😢", delete_after=5.0)
+        elif isinstance(error, BadArgument):
+            await ctx.send("Erm~, sorry- I can't find that member...", delete_after=5.0)
+
+
+def setup(bot):
+    bot.add_cog(HighFive(bot))
