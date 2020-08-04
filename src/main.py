54e847fc4da43dd058db52f649e3c11a572f3f88
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-08-04 13:08:48 +0200

    Setting per guild prefix

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from src.utils import get_key_from_json, load_cogs
-import src.serverconfig
+from src.serverconfig import check_server_json, get_guild_prefix
 
 
 class Chan(commands.Bot):
@@ -9,15 +9,16 @@ class Chan(commands.Bot):
         super().__init__(command_prefix=prefix)
 
     async def on_ready(self):
-        src.serverconfig.check_server_json(self)  # Check if each servers where the bot is, have a json config file
+        check_server_json(self)  # Check if each servers where the bot is, have a json config file
         load_cogs(self, subdir='commands')  # Loading all commands
         load_cogs(self, subdir='commands/social')  # Loading all social commands
         load_cogs(self, subdir='commands/mod')  # Loading all moderation commands
+        load_cogs(self, subdir='commands/game_stats')  # Loading all game stats commands
         print(f"Logged as {self.user}, active in {len(self.guilds)} server(s) with a total amount of {len([user for user in self.users if not user.bot])} user(s).")
 
 
 if __name__ == '__main__':
-    client = Chan(prefix='%')
+    client = Chan(prefix=get_guild_prefix)
     # Middleware
     client.remove_command('help')
     client.run(get_key_from_json("TOKEN"))
