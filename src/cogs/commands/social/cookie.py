250c0e8a58dabd39ec1964c93919f7f0902369a2
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-08-06 11:46:19 +0200

    Fix typo (#22)

--- src/cogs/commands/social/cookie.py
+++ src/cogs/commands/social/cookie.py
@@ -19,7 +19,7 @@ class Cookie(commands.Cog):
     async def cookie(self, ctx, target: User):
         await safe_delete(ctx)
         if target == self.bot.user:
-            await ctx.send(f"Thankkks ! I love cookie ! I love you sooo muchhh ! (%luv)")
+            await ctx.send(f"Thankkks ! I love cookies ! I love you sooo muchhh ! (%luv)")
         elif ctx.message.author != target:
             await ctx.send(f"👉 👈 Hey {target.mention}, {ctx.message.author.mention} gave you a cookie ! 🍪")
         else: await ctx.send("Do not be egoist ! (Give me that cookie)", delete_after=5.0)
