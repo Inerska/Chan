b2c3212be79700b2482eb4271cda5ee5f3f13403
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-08-07 19:08:19 +0200

    Smug social command

---
+++ src/cogs/commands/social/smug.py
@@ -0,0 +1,25 @@
+from discord import Embed
+from discord.ext import commands
+
+from src.modules.random import get_random_gif_by_theme
+
+
+# Hehe.. :3
+from src.utils import safe_delete
+
+
+class Smug(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='smug')
+    async def smug(self, ctx):
+        await safe_delete(ctx)
+        await ctx.send(embed=Embed(description=f"¬‿¬ **{ctx.author.name}** is preparing something- ",
+                                   color=0x2F3136)
+                       .set_image(url=f"{await get_random_gif_by_theme('anime smug')}"))
+
+
+def setup(bot):
+    bot.add_cog(Smug(bot))
